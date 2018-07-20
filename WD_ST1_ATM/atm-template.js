be2b4bf4a6886aa78b06d823acffa829e6747895
Konstantin Yakimenko <kost.yakimenko@gmail.com>
2018-07-20 22:04:03 +0300

    create function logging()

--- WD_ST1_ATM/atm-template.js
+++ WD_ST1_ATM/atm-template.js
@@ -43,150 +43,123 @@ const ATM = {
     /* Authorization */
     auth: function(number, pin) {
         if (this.is_auth) {
-            this.addReport(this.message.reportQueueErr);
-            console.warn(this.message.queueErr);
-            return false;
+            return this.logging(this.message.reportQueueErr, this.message.queueErr, false);
         }
 
         if (typeof number !== 'string' || typeof pin !== 'string') {
-            this.addReport(this.message.reportParamErr);
-            console.warn(this.message.paramErr);
-            return false;
+            return this.logging(this.message.reportParamErr, this.message.paramErr, false);
         }
 
-        const user = this.users.find((user) => {return user.number === number && user.pin === pin});
+        this.current_user = this.users.find((user) => {return user.number === number && user.pin === pin});
 
-        if (user !== undefined) {
+        if (this.current_user !== undefined) {
             this.is_auth = true;
-            this.current_user = user;
-            this.current_type = user.type;
-            this.addReport(`${this.current_type} ${this.current_user.number} ${this.message.reportAuthOk}`);
-            console.info(this.message.authOk);
-            return true;
+            this.current_type = this.current_user.type;
+            return this.logging(`${this.current_type} ${this.current_user.number} ${this.message.reportAuthOk}`,
+                                this.message.authOk, true);
         }
 
-        this.addReport(this.message.reportUserNotFound);
-        console.warn(this.message.userNotFound);
-        return false;
+        return this.logging(this.message.reportUserNotFound, this.message.userNotFound, false);
     },
 
     /* Check current debet */
     check: function() {
         if (!this.is_auth) {
-            this.addReport(this.message.reportUnauthorized);
-            console.warn(this.message.unauthorized);
-            return false;
+            return this.logging(this.message.reportUnauthorized, this.message.unauthorized, false);
         }
 
-        this.addReport(`${this.current_type} ${this.current_user.number} ${this.message.reportCheck}`);
-        console.info(`Current debet: ${this.current_user.debet}`);
-        return true;
+        return this.logging(`${this.current_type} ${this.current_user.number} ${this.message.reportCheck}`,
+                            `Current debet: ${this.current_user.debet}`, true);
     },
 
     /* Get cash - available for user only */
     getCash: function(amount) {
         if (this.current_type !== 'user') {
-            this.addReport(this.message.reportAccessErr);
-            console.warn(this.message.userAccessErr);
-            return false;
+            return this.logging(`getCash ${this.message.reportAccessErr}`, this.message.userAccessErr, false);
         }
 
         if (!Number.isInteger(amount) || amount < 0) {
-            this.addReport(this.message.reportAmountErr);
-            console.warn(this.message.amountErr);
-            return false;
+            return this.logging(this.message.reportAmountErr, this.message.amountErr, false);
         }
 
         if (amount > this.current_user.debet) {
-            this.addReport(this.message.reportUserCashDeficit);
-            console.warn(this.message.userCashDeficit);
-            return false;
+            return this.logging(this.message.reportUserCashDeficit, this.message.userCashDeficit, false);
         }
 
         if (amount > this.cash) {
-            this.addReport(this.message.reportAtmCashDeficit);
-            console.warn(this.message.atmCashDeficit);
-            return false;
+            return this.logging(this.message.reportAtmCashDeficit, this.message.atmCashDeficit, false);
         }
 
         this.current_user.debet -= amount;
         this.cash -= amount;
 
-        this.addReport(`user ${this.current_user.number} get ${amount}`);
-        console.info(`You get ${amount}`);
-        return true;
+        return this.logging(`user ${this.current_user.number} get ${amount}`, `You get ${amount}`, true);
     },
 
     /* Load cash - available for user only */
     loadCash: function(amount){
         if (this.current_type !== 'user') {
-            this.addReport(this.message.reportAccessErr);
-            console.warn(this.message.userAccessErr);
-            return false;
+            return this.logging(`loadCash ${this.message.reportAccessErr}`, this.message.userAccessErr, false);
         }
 
         if (!Number.isInteger(amount) || amount < 0) {
-            this.addReport(this.message.reportAmountErr);
-            console.warn(this.message.amountErr);
-            return false;
+            return this.logging(this.message.reportAmountErr, this.message.amountErr, false);
         }
 
         this.current_user.debet += amount;
         this.cash += amount;
 
-        this.addReport(`user ${this.current_user.number} load ${amount}`);
-        console.info(`You load ${amount}`);
-        return true;
+        return this.logging(`user ${this.current_user.number} load ${amount}`, `You load ${amount}`, true);
     },
 
     /* Load cash to ATM - available for admin only */
     load_cash: function(addition) {
         if (this.current_type !== 'admin') {
-            this.addReport(this.message.reportAccessErr);
-            console.warn(this.message.adminAccessErr);
-            return false;
+            return this.logging(`load_cash ${this.message.reportAccessErr}`, this.message.adminAccessErr, false);
         }
 
         if (!Number.isInteger(addition) || addition < 0) {
-            this.addReport(this.message.reportAmountErr);
-            console.warn(this.message.amountErr);
-            return false;
+            return this.logging(this.message.reportAmountErr, this.message.amountErr, false);
         }
 
         this.cash += addition;
-        this.addReport(`admin load ${addition}`);
-        console.info(`ATM cash ${this.cash}`);
-        return true;
+
+        return this.logging(`admin load ${addition}`, `Admin load ${addition}`, true);
     },
 
     /* Get report about cash actions - available for admin only */
     getReport: function() {
         if (this.current_type !== 'admin') {
-            console.warn(this.message.adminAccessErr);
-            return false;
+            return this.logging(`getReport ${this.message.reportAccessErr}`, this.message.adminAccessErr, false);
         }
 
         this.reports.forEach((item) => (console.info(`${item.date}: ${item.event}`)));
+
         return true;
     },
 
     /* Log out */
     logout: function() {
         if (!this.is_auth) {
-            this.addReport(this.message.reportUnauthorized);
-            console.warn(this.message.unauthorized);
-            return false;
+            return this.logging(this.message.reportUnauthorized, this.message.unauthorized, false);
         }
 
-        this.addReport(`${this.current_type} ${this.current_user.number} is logout`);
+        this.logging(`${this.current_type} ${this.current_user.number} is logout`, this.message.logOut, true);
         this.is_auth = this.current_user = this.current_type = false;
 
-        console.info(this.message.logOut);
         return true;
     },
 
-    /* Add report */
-    addReport: function(message) {
-        this.reports.push({date: new Date().toLocaleString('en-GB'), event: message});
+    /* Add report and console logging */
+    logging: function(reportMsg, userMsg, isCorrect) {
+        this.reports.push({date: new Date().toLocaleString('en-GB'), event: reportMsg});
+
+        if (isCorrect) {
+            console.info(userMsg);
+        } else {
+            console.warn(userMsg);
+        }
+
+        return isCorrect;
     }
 };
