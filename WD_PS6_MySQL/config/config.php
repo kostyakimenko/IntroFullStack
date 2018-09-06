e5ad57508cf356068bf946b27e72eaf1cc2b883f
Konstantin Yakimenko <kost.yakimenko@gmail.com>
2018-09-06 15:39:33 +0300

    change name param connect => database

--- WD_PS6_MySQL/config/config.php
+++ WD_PS6_MySQL/config/config.php
@@ -7,14 +7,14 @@ define ('TEMPLATES_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app' . DIREC
 return [
     'authorization' => HANDLERS_PATH . DIRECTORY_SEPARATOR . 'authorization.php',
     'messaging' => HANDLERS_PATH . DIRECTORY_SEPARATOR . 'messaging.php',
-    'classLoader' => SERVICES_PATH . DIRECTORY_SEPARATOR . 'ClassLoader.php',
+    'classLoader' => SERVICES_PATH . DIRECTORY_SEPARATOR . 'class-loader.php',
     'header' => TEMPLATES_PATH . DIRECTORY_SEPARATOR . 'header.php',
     'footer' => TEMPLATES_PATH . DIRECTORY_SEPARATOR . 'footer.php',
     'auth-block' => TEMPLATES_PATH . DIRECTORY_SEPARATOR . 'auth-block.php',
     'chat-block' => TEMPLATES_PATH . DIRECTORY_SEPARATOR . 'chat-block.php',
     'db-error' => TEMPLATES_PATH . DIRECTORY_SEPARATOR . 'db-error.php',
 
-    'connect' => [
+    'database' => [
         'hostname' => 'localhost',
         'username' => 'user',
         'password' => '0000',
