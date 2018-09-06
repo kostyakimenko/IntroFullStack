dce4d1da4e70888f6bedf625ca3cbbe56ec674fc
Konstantin Yakimenko <kost.yakimenko@gmail.com>
2018-09-06 20:59:26 +0300

    add sql script

---
+++ WD_PS6_MySQL/chat_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id int(10) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username varchar(30) NOT NULL,
+    password varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE UNIQUE INDEX users_username_uindex ON users (username);
+
+CREATE TABLE messages (
+    id int(10) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message varchar(255) NOT NULL,
+    user varchar(30) NOT NULL,
+    time timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
